7dc52a049ae00bce0e3aae6c3369de98dae58f75
kietalang <kietalang.dev@gmail.com>
2020-12-25 09:20:09 +0700

    Complete exercise 32 add the same file with exercise 31 with topic find the least common multiple

--- Python exercises/Python basic/exercise31.py
+++ Python exercises/Python basic/exercise31.py
@@ -2,22 +2,25 @@
 # #Import math Library
 # import math
 
-# #find the  the greatest common divisor of the two integers
-# print (math.gcd(3, 6))
+# find the greatest common divisor of the two integers
 
 def gcd(a, b):
-    # if a = 0 => gcd(a,b) = b
-    # if b = 0 => gcd(a,b) = a
+    # if a = 0 => gcd(a,b) = b or reverse
     if (a == 0 or b == 0):
         return a + b
 
     while (a != b):
         if (a > b):
-            a -= b # a = a - b
+            a -= b
         else:
             b -= a
    
     return a # return a or b because a and b has the same values now
 
+# find the least common multiple
+def lcm(a, b):
+    return int((a / gcd(a, b)) * b)
+
 
 print(gcd(3, 6))
+print(lcm(3, 6))
